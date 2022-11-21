184c6106cf0601ec85b9da3d97e6e6b38c3b7111
edemkaaa <113183510+edemkaaa@users.noreply.github.com>
2022-11-21 21:28:04 +0300

    6.2

---
+++ data.js
@@ -0,0 +1,22 @@
+var data = [{
+title: 'Хэтчбек',
+nick: 'hatchback',
+avatar: '/images/hatchback.jpeg',
+desc: 'Приора,которая подойдет всем - и груз загрузить,и поставить музыку,и покататься.'
+},
+{
+title: 'Седан',
+nick: 'sedan',
+avatar: '/images/sedan.png',
+desc: 'Приора,которая самая молодежная. Её тонируют,и выглядит она лучше всех.'
+},
+{
+title: 'Универсал',
+nick: 'universal',
+avatar: '/images/universal.png',
+desc: 'Приора для дедушек,либо рабочих - в ней можно возить много груза'
+}
+];
+
+
+module.exports.data = data;
