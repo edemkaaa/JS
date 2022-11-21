0348baca67b4c363125ac9cbd8b06855b121d758
edemkaaa <113183510+edemkaaa@users.noreply.github.com>
2022-11-21 22:32:48 +0300

    7.3

--- mongooseTasks.js
+++ mongooseTasks.js
@@ -1,15 +1,16 @@
 var mongoose = require('mongoose')
-mongoose.connect('mongodb://127.0.0.1:27017/test')
+mongoose.connect('mongodb://127.0.0.1:27017/test1')
+var Car = require("./models/car").Car
 
-var schema = mongoose.Schema({ name: String })
 
-schema.methods.brrr = function(){
-    console.log(this.get("name") + " сказал брр")
-}
+var car = new Car({
+    title: "Седан",
+    nick: "sedan"
+})
 
-var Car = mongoose.model('Car', schema)
 
-var bunker = new Car({ name: 'приора' })
-bunker.save(function (err) {
-    bunker.brrr()
+
+console.log(car)
+car.save(function(err, car, affected){
+console.log(car.title)
 })
