4e95b9ce1552c5a8628c92fefd4717c86008fc0b
Elpadra <113183510+edemkaaa@users.noreply.github.com>
2022-12-07 20:09:16 +0300

    9.2

--- routes/index.js
+++ routes/index.js
@@ -5,13 +5,18 @@ var Priora = require("../models/priora").Priora
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+    res.cookie('greeting', 'Hi!!!').render('index', { title: 'Express', menu:menu });
+});
+
+/* GET home page. 
+router.get('/', function(req, res, next) {
     Priora.find({},{_id:0,title:1,nick:1},function(err,menu){
         res.render('index', {
                                 title: 'Express',
                                 menu: menu
                             });
     })
-
 });
+*/
 
 module.exports = router;
