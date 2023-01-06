2e6d38948087eddf4de6e0fa528afe6ba32dcd00
Elpadra <Elpadraaa@gmail.com>
2023-01-06 21:21:41 +0300

    11.4

--- middleware/createMenu.js
+++ middleware/createMenu.js
@@ -1,13 +1,14 @@
 var Priora = require("./../models/priora").Priora
 
 
+
 module.exports = function(req,res,next){
-    res.locals.nav = []
+res.locals.nav = []
 
-    Priora.find(null,{_id:0,title:1,nick:1},function(err,result){
-        if(err) throw err
-        res.locals.nav = result
-        next()
-    })
 
+Priora.find(null,{_id:0,title:1,nick:1},function(err,result){
+if(err) throw err
+res.locals.nav = result
+next()
+})
 }
