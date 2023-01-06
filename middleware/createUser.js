c231c5ff9a6456e2df5db4ed34cf4ea8edb5c19d
Elpadra <Elpadraaa@gmail.com>
2023-01-06 21:42:22 +0300

    11.5

--- middleware/createUser.js
+++ middleware/createUser.js
@@ -1,12 +1,18 @@
-var User = require("./../models/user").User
+var user = require("./../models/user").user
 
 module.exports = function(req,res,next) {
-    res.locals.user = null
+res.locals.user = null
 
-    User.findById(req.session.user, function (err, user) {
-        if (err)
-            return next(err)
-        res.locals.user = user;
-        next();
-    })
+
+// User.findById(req.session.user, function (err, user) {
+// if (err)
+// return next(err)
+// res.locals.user = user;
+// next();
+// })
+
+
+next();
+
+
 }
